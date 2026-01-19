af05cbbab023e9d54f629001d15671d853940272
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2026-01-19 08:25:12 -0300

    Update install_libs.sh

--- resources/install_libs.sh
+++ resources/install_libs.sh
@@ -3,7 +3,7 @@
 
 
 pip3 install opencv-python opencv-contrib-python cvzone mediapipe flpiotlib
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
 pip3 install -r requirements.txt
 
